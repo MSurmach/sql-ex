2efbd38c79363a4547357941f458278ceb4acc80
m.surmach <maximsurmach@gmail.com>
2022-05-12 09:49:23 +0300

    Create folder

---
+++ SELECT (learning stage)/#30.sql
@@ -0,0 +1,3 @@
+-- Under the assumption that receipts of money (inc) and payouts (out) can be registered any number of times a day for each collection point [i.e. the code column is the primary key], display a table with one corresponding row for each operating date of each collection point.
+-- Result set: point, date, total payout per day (out), total money intake per day (inc).
+-- Missing values are considered to be NULL.
